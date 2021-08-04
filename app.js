8ff9b83e444cdc7247eeed38e4a95aff68c3702b
Yash Patel <yashpatel9090@gmail.com>
2021-08-04 14:12:29 +0530

    taking input from the user

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello Guys");
+var btnTrans = document.querySelector('#btn-click');
+
+var texInput = document.querySelector('#text-input');
+
+ console.log(texInput);
+
+function clickHandler(){
+    console.log("clicked");
+    console.log("input",texInput.value);
+};
+
+btnTrans.addEventListener("click", clickHandler);
